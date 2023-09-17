8c67da130029adf6f0fafe3a1d7ac0e9a1f510cd
YeJi222 <hhongyizi@gmail.com>
2023-09-17 23:01:25 +0900

    feat: mysite03(spring) - structure

--- mysite03/src/main/java/com/poscodx/mysite/controller/GuestbookController.java
+++ mysite03/src/main/java/com/poscodx/mysite/controller/GuestbookController.java
@@ -1,29 +1,44 @@
-package com.poscodx.mysite.controller;
+package com.poscodx.guestbook.controller;
 
+import com.poscodx.guestbook.repository.GuestbookRepository;
+import com.poscodx.guestbook.vo.GuestbookVo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.poscodx.mysite.service.GuestbookService;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/guestbook")
 public class GuestbookController {
 	@Autowired
 	private GuestbookService guestbookService;
-	
+
 	@RequestMapping("")
-	public String main() {
-		List<GuestbookVo> list = guestbookService.getContentList();
+	public String main(Model model) {
+		List<GuestbookVo> list = guestbookService.getContentsList();
 		model.addAttribute("list", list);
-		
 		return "guestbook/main";
 	}
-	
-	@RequestMapping("/delete/{no}")
-	public String delete(@PathVariable("no") Long no) {
-		return "guestbook/deleteform";
-	}
+
+//	@RequestMapping("/add") // add action
+//	public String add(GuestbookVo vo) {
+//		guestbookRepository.insert(vo);
+//		return "redirect:/";
+//	}
+//
+//	@RequestMapping(value="/delete/{no}", method=RequestMethod.GET) // delete form
+//	public String delete(@PathVariable("no") Long no, Model model) {
+//		model.addAttribute("no", no);
+//		return "delete";
+//	}
+//
+//	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST) // delete action
+//	public String delete(@PathVariable("no") Long no, @RequestParam(value="password", required=true, defaultValue="") String password) {
+//		guestbookRepository.deleteByNoAndPassword(no, password);
+//		return "redirect:/";
+//	}
 }
--- mysite03/src/main/java/com/poscodx/mysite/vo/GuestbookVo.java
+++ mysite03/src/main/java/com/poscodx/mysite/vo/GuestbookVo.java
@@ -1,5 +1,44 @@
-package com.poscodx.mysite.vo;
+package com.poscodx.guestbook.vo;
 
 public class GuestbookVo {
-
+	private Long no;
+	private String name;
+	private String contents;
+	private String password;
+	private String regDate;
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	@Override
+	public String toString() {
+		return "GuestbookVo [no=" + no + ", name=" + name + ", contents=" + contents + ", password=" + password
+			+ ", regDate=" + regDate + "]";
+	}
 }
