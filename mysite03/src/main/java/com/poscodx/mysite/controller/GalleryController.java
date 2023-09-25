670bc5e65d80216122b49d66df54945f69c874c0
YeJi222 <hhongyizi@gmail.com>
2023-09-25 12:31:49 +0900

    feat: lecture-gallery #2

--- mysite03/src/main/java/com/poscodx/mysite/controller/GalleryController.java
+++ mysite03/src/main/java/com/poscodx/mysite/controller/GalleryController.java
@@ -1,17 +1,24 @@
 package com.poscodx.mysite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.poscodx.mysite.security.Auth;
+import com.poscodx.mysite.service.GalleryService;
 
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
+	@Autowired
+	private GalleryService galleryService;
 	
 	@RequestMapping("")
 	public String index() {
 		return "gallery/index";
 	}
 	
+	@Auth(Role="ADMIN")
 	@RequestMapping("/upload")
 	public String upload() {
 		
@@ -20,6 +27,7 @@ public class GalleryController {
 	}
 	
 	// delete 
+	// @Auth(Role="ADMIN")
 	// @RequestMapping("")
 	
 }
---
+++ mysite03/src/main/java/com/poscodx/mysite/service/GalleryService.java
@@ -0,0 +1,23 @@
+package com.poscodx.mysite.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.poscodx.mysite.vo.GalleryVo;
+
+@Service
+public class GalleryService {
+	public List<GalleryVo> getImages(){
+		
+		return null;
+	}
+	
+	public void removeImage(Long no) {
+		
+	}
+	
+	public void addImage(GalleryVo vo) {
+		
+	}
+}
